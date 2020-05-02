1b0e8f35497b0879f1c798bdd5268c0af86ebefe
RagePeanut <dorian.gruselin@hotmail.com>
2020-05-02 17:44:17 +0200

    Fix: simplified class/id names

--- src/typescript/html_helper.ts
+++ src/typescript/html_helper.ts
@@ -3,46 +3,46 @@ import * as $ from 'jquery';
 import { Mention } from './models/mention';
 
 export class HtmlHelper {
-    private static _mentionsDialog: string;
-    private static _mentionOverview: string;
-    private static _mentionSuggestion: string;
-    private static _mentionExtract: string;
+    private static _dialog: string;
+    private static _overview: string;
+    private static _suggestion: string;
+    private static _extract: string;
 
     private static mentionIndex: number;
 
     static async init() {
-        HtmlHelper._mentionsDialog = await $.get(chrome.runtime.getURL("html/mentions_dialog.html"));
-        HtmlHelper._mentionOverview = await $.get(chrome.runtime.getURL("html/mention_overview.html"));
-        HtmlHelper._mentionSuggestion = await $.get(chrome.runtime.getURL("html/mention_suggestion.html"));
-        HtmlHelper._mentionExtract = await $.get(chrome.runtime.getURL("html/mention_extract.html"));
+        HtmlHelper._dialog = await $.get(chrome.runtime.getURL("html/mentions_dialog.html"));
+        HtmlHelper._overview = await $.get(chrome.runtime.getURL("html/mention_overview.html"));
+        HtmlHelper._suggestion = await $.get(chrome.runtime.getURL("html/mention_suggestion.html"));
+        HtmlHelper._extract = await $.get(chrome.runtime.getURL("html/mention_extract.html"));
     }
 
     static mentionsDialog(mentions: Mention[], onClose: VoidCallback): HTMLElement {
         HtmlHelper.mentionIndex = 0;
-        const dialog: Node = $.parseHTML(HtmlHelper._mentionsDialog)[0];
-        let mentionOverviewElt: HTMLElement = HtmlHelper.mentionOverview(mentions[0]);
+        const dialog: Node = $.parseHTML(HtmlHelper._dialog)[0];
+        let overview: HTMLElement = HtmlHelper.mentionOverview(mentions[0]);
         const jqDialog: JQuery<Node> = $(dialog);
-        jqDialog.find("#checky__mentions-overview").append(mentionOverviewElt);
+        jqDialog.find("#checky__dialog-body").append(overview);
 
-        jqDialog.find("#checky__previous-button").click(() => {
+        jqDialog.find("#checky__previous").click(() => {
             if(HtmlHelper.mentionIndex > 0) {
                 const focusedMention: Mention = mentions[--HtmlHelper.mentionIndex];
-                const newMentionOverviewElt: HTMLElement = HtmlHelper.mentionOverview(focusedMention);
-                $(mentionOverviewElt).replaceWith(newMentionOverviewElt);
-                mentionOverviewElt = newMentionOverviewElt;
+                const newOverview: HTMLElement = HtmlHelper.mentionOverview(focusedMention);
+                $(overview).replaceWith(newOverview);
+                overview = newOverview;
             }
         });
 
-        jqDialog.find("#checky__next-button").click(() => {
+        jqDialog.find("#checky__next").click(() => {
             if(HtmlHelper.mentionIndex < mentions.length - 1) {
                 const focusedMention: Mention = mentions[++HtmlHelper.mentionIndex];
-                const newMentionOverviewElt: HTMLElement = HtmlHelper.mentionOverview(focusedMention);
-                $(mentionOverviewElt).replaceWith(newMentionOverviewElt);
-                mentionOverviewElt = newMentionOverviewElt;
+                const newOverview: HTMLElement = HtmlHelper.mentionOverview(focusedMention);
+                $(overview).replaceWith(newOverview);
+                overview = newOverview;
             }
         });
 
-        jqDialog.find("#checky__done-button").click(() => {
+        jqDialog.find("#checky__done").click(() => {
             jqDialog.remove();
             onClose()
         });
@@ -56,21 +56,21 @@ export class HtmlHelper {
     }
 
     private static mentionOverview(mention: Mention): HTMLElement {
-        const overview: HTMLElement = $.parseHTML(HtmlHelper._mentionOverview.replace(/%mention%/g, mention.raw))[0] as HTMLElement;
+        const overview: HTMLElement = $.parseHTML(HtmlHelper._overview.replace(/%mention%/g, mention.raw))[0] as HTMLElement;
         const select: HTMLElement = $("<select></select>").append(HtmlHelper.mentionSuggestions(mention.suggestions)).get()[0];
-        $(overview).find(".checky__mention-overview-suggestions").append(select);
-        $(overview).find(".checky__mention-overview-extracts").append(HtmlHelper.mentionExtracts(mention.extracts));
+        $(overview).find("#checky__suggestions").append(select);
+        $(overview).find("#checky__extracts").append(HtmlHelper.mentionExtracts(mention.extracts));
         return overview as HTMLElement;
     }
 
     private static mentionSuggestions(suggestions: string[]): HTMLElement[] {
-        const options: string = suggestions.map(suggestion => HtmlHelper._mentionSuggestion.replace(/%suggestion%/g, suggestion))
+        const options: string = suggestions.map(suggestion => HtmlHelper._suggestion.replace(/%suggestion%/g, suggestion))
                                            .join("");
         return $(options).get();
     }
 
     private static mentionExtracts(extracts: string[]): HTMLElement[] {
-        const divs: string = extracts.map((extract, index) => HtmlHelper._mentionExtract.replace(/%extract_number%/g, (index + 1).toString())
+        const divs: string = extracts.map((extract, index) => HtmlHelper._extract.replace(/%extract_number%/g, (index + 1).toString())
                                                                                         .replace(/%extract%/g, extract))
                                      .join("");
         return $(divs).get();
